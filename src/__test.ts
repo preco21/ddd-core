140502ff6542451393c8b6512995bd1b7bc30a65
Plusb Preco <plusb21@gmail.com>
2021-07-28 01:44:41 +0900

    feat: some changes on example

--- src/__test.ts
+++ src/__test.ts
@@ -35,19 +35,30 @@ class FooEntity extends Entity<FooEntityProps> {
   }
 }
 
+const a = new BarVO({
+  name: 'foo',
+  description: 'foo',
+  createdAt: new Date(),
+});
+const b = new BarVO({
+  name: 'foo',
+  description: 'foo',
+  createdAt: new Date(),
+});
+
+a.equals(b); // structural equality
+a.getRaw();
+a.toJSON();
+console.log(a.getFull());
+
 const c = new FooEntity(new FooEntityProps({
-  bar: new BarVO({
-    name: '나나나',
-    description: 'foo',
-    createdAt: new Date(),
-  }),
+  bar: a,
 }), '123');
 const d = new FooEntity({
-  bar: new BarVO({
-    name: '나나나',
-    description: 'foo',
-    createdAt: new Date(),
-  }),
+  bar: b,
 }, '321');
 
-c.equals(d);
+c.equals(d); // referential equality
+c.toJSON();
+c.isTransient();
+console.log(c.id, c.key, c.createdAt);
